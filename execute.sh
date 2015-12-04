155a69eec02e6be0189a2dd6375e660f39166a70
Michael <michaelh@tune.com>
2015-12-04 20:23:28 +0000

    Make ENV inclusion optional parameter to execute

--- execute.sh
+++ execute.sh
@@ -13,8 +13,13 @@ export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile
 
 CONFIG_ENV_FILE=./config.env
 
-if [ -f $CONFIG_ENV_FILE ]; then
-    source $CONFIG_ENV_FILE
+if [ -n "$2" ]
+  then
+    if [ -f "./$2" ]; then
+      set -a
+      source ./$2
+      set +a
+    fi
 fi
 
 go run listing.go
