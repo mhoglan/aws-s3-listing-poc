092f1925face3fb838047349e5aa4c9b6ef09e45
Michael <michaelh@tune.com>
2015-12-04 19:48:45 +0000

    Added ability to have local config file for ENV inclusion

--- execute.sh
+++ execute.sh
@@ -10,4 +10,11 @@ PROFILE=$1
 
 export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile $PROFILE)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile $PROFILE)
+
+CONFIG_ENV_FILE=./config.env
+
+if [ -f $CONFIG_ENV_FILE ]; then
+    source $CONFIG_ENV_FILE
+fi
+
 go run listing.go
